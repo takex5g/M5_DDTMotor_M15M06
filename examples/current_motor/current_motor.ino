6f94a06d7a16e2a204405533fd3da80b557d745e
takex5g <takex5g@gmail.com>
2022-06-03 00:28:25 +0900

    ✨ 電流モードサンプル追加

---
+++ examples/current_motor/current_motor.ino
@@ -0,0 +1,62 @@
+#include <DDT_Motor_M15M06.h>
+#include <M5StickCPlus.h>
+
+int16_t Speed = 0;   // Speed of motor
+uint8_t Acce = 0;    // Acceleration of motor
+uint8_t Brake_P = 0; // Brake position of motor
+uint8_t ID = 1;      // ID of Motor (default:1)
+
+Receiver Receiv;
+// M5Stackのモジュールによって対応するRX,TXのピン番号が違うためM5製品とRS485モジュールに対応させてください
+auto motor_handler = MotorHandler(33, 32); // RX,TX
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("DDT-Motor RS485");
+  delay(100);
+  motor_handler.Control_Motor(0, ID, Acce, Brake_P, &Receiv); //モータ停止
+  delay(100);
+  uint8_t Mode = 0x01;                   // current loop
+  motor_handler.Set_MotorMode(Mode, ID); //モード変更
+  delay(100);
+}
+
+const int16_t SPEED_MAX = 32767 - 100;
+const int16_t SPEED_MIN = -32767 + 100;
+void loop()
+{
+  while (true)
+  {
+    Speed += 50;
+    delay(5);
+    motor_handler.Control_Motor(Speed, ID, Acce, Brake_P, &Receiv);
+    Serial.print(" Mode:");
+    Serial.print(Receiv.BMode);
+    Serial.print(" Current:");
+    Serial.print(Receiv.ECurru);
+    Serial.print(" Speed:");
+    Serial.println(Receiv.BSpeed);
+    if (Speed > int16_t(SPEED_MAX))
+    {
+      break;
+    }
+  }
+
+  while (true)
+  {
+    Speed -= 50;
+    delay(5);
+    motor_handler.Control_Motor(Speed, ID, Acce, Brake_P, &Receiv);
+    Serial.print(" Mode:");
+    Serial.print(Receiv.BMode);
+    Serial.print(" Current:");
+    Serial.print(Receiv.ECurru);
+    Serial.print(" Speed:");
+    Serial.println(Receiv.BSpeed);
+    if (Speed < SPEED_MIN)
+    {
+      break;
+    }
+  }
+}
